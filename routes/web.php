493dc50b5a99e00f93091211d872ab6a32769933
Yusa Darmawan <yusadarmawan5@gmail.com>
2022-09-30 09:13:33 +0800

    edit 1

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('task');
+});
+
+Route::post('/task', function (Request $request) {
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+});
